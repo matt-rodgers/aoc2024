299c2e98db9630055762ef59d44bfe66b200b0a9
Matt Rodgers <mattrodgers31@gmail.com>
2025-03-03 21:28:06 +0000

    refactor day15 pt1 to use recursive move solution

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, time::Instant};
+use std::{fmt::Display, panic::Location, time::Instant};
 
 fn main() {
     let input = include_str!("../../inputs/15.in");
@@ -115,72 +115,43 @@ impl Display for Grid {
 }
 
 impl Grid {
+    fn apply_move_inner(&mut self, (x, y): (usize, usize), mv: Move) -> Result<(), MyError> {
+        let (nx, ny) = match mv {
+            Move::Up => (x, y - 1),
+            Move::Down => (x, y + 1),
+            Move::Right => (x + 1, y),
+            Move::Left => (x - 1, y),
+        };
+
+        let current_element = self.elements[y][x];
+        let next_element = self.elements[ny][nx];
+
+        let res = match next_element {
+            GridElement::Wall => return Err(MyError),
+            GridElement::Free => Ok(()),
+            GridElement::Box => self.apply_move_inner((nx, ny), mv),
+            GridElement::Robot => panic!("Multiple robots in grid!"),
+        };
+
+        // Element at current location might not actually end up free. But if this is the case it
+        // will be overwritten with the correct value after this function returns.
+        self.elements[y][x] = GridElement::Free;
+        self.elements[ny][nx] = current_element;
+
+        if current_element == GridElement::Robot {
+            self.robot_loc = (nx, ny);
+        }
+
+        res
+    }
+
     fn apply_move(&mut self, mv: Move) {
-        let (next_x, next_y) = match mv {
-            Move::Up => (self.robot_loc.0, self.robot_loc.1 - 1),
-            Move::Down => (self.robot_loc.0, self.robot_loc.1 + 1),
-            Move::Right => (self.robot_loc.0 + 1, self.robot_loc.1),
-            Move::Left => (self.robot_loc.0 - 1, self.robot_loc.1),
-        };
+        let mut new_self = self.clone();
 
-        let next_element = self.elements[next_y][next_x];
-
-        match next_element {
-            GridElement::Free => {
-                self.elements[next_y][next_x] = GridElement::Robot;
-                self.elements[self.robot_loc.1][self.robot_loc.0] = GridElement::Free;
-                self.robot_loc = (next_x, next_y);
-            }
-            GridElement::Wall => {
-                // Can't move
-            }
-            GridElement::Box => {
-                // Now we need to see if the box can be pushed forward...
-                // Check grid squares in the relevant direction until we find:
-                // - a free square: in which case put a box there an move the robot forward
-                // - a wall: in which case we cannot move and should stop checking
-                let (mut x, mut y) = (next_x, next_y);
-                match mv {
-                    Move::Up => {
-                        while let GridElement::Box = self.elements[y][x] {
-                            y -= 1;
-                        }
-                    }
-                    Move::Down => {
-                        while let GridElement::Box = self.elements[y][x] {
-                            y += 1;
-                        }
-                    }
-                    Move::Right => {
-                        while let GridElement::Box = self.elements[y][x] {
-                            x += 1;
-                        }
-                    }
-                    Move::Left => {
-                        while let GridElement::Box = self.elements[y][x] {
-                            x -= 1;
-                        }
-                    }
-                }
-
-                match self.elements[y][x] {
-                    GridElement::Wall => {
-                        // Can't move, do nothing
-                    }
-                    GridElement::Free => {
-                        // Place a box here and move the robot forward
-                        self.elements[y][x] = GridElement::Box;
-                        self.elements[next_y][next_x] = GridElement::Robot;
-                        self.elements[self.robot_loc.1][self.robot_loc.0] = GridElement::Free;
-                        self.robot_loc = (next_x, next_y);
-                    }
-                    _ => panic!("There shouldn't be a box or a robot here..."),
-                }
-            }
-            GridElement::Robot => {
-                panic!("Can't be more than one robot...");
-            }
-        };
+        if new_self.apply_move_inner(self.robot_loc, mv).is_ok() {
+            self.elements = new_self.elements;
+            self.robot_loc = new_self.robot_loc;
+        }
     }
 
     fn gps_score(&self) -> u64 {
