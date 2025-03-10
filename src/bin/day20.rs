8c32b8418ae5a9291c0759c2df42f76f388ec8e5
Matt Rodgers <mattrodgers31@gmail.com>
2025-03-10 08:22:33 +0000

    day20 pt2

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -13,10 +13,12 @@ fn main() {
 }
 
 fn run(input: &str) -> (u64, u64) {
-    run_inner(input, 100)
+    let pt1 = run_inner(input, 2, 100);
+    let pt2 = run_inner(input, 20, 100);
+    (pt1, pt2)
 }
 
-fn run_inner(input: &str, threshold: u32) -> (u64, u64) {
+fn run_inner(input: &str, cheat_time: usize, threshold: u32) -> u64 {
     let mut end = (0, 0);
     let mut unvisited: PriorityQueue<(usize, usize), Reverse<u32>> = PriorityQueue::new();
     let mut wall_tiles: Vec<(usize, usize)> = Vec::new();
@@ -79,53 +81,49 @@ fn run_inner(input: &str, threshold: u32) -> (u64, u64) {
     }
     assert_eq!(max_cost_pos, end);
 
-    // Then for each two distinct positions on the course separated by a single wall tile, check
-    // how much time would be saved by going through the wall.
-    let mut pt1 = 0;
-    for wall_tile in wall_tiles.iter() {
-        // Check if the wall tile separates two positions on the course, vertically or horizontally
-        if wall_tile.0 > 0 {
-            if let Some(time_saved) = time_saved_by_shortcut(
-                &(wall_tile.0 - 1, wall_tile.1),
-                &(wall_tile.0 + 1, wall_tile.1),
-                &visited,
-            ) {
-                if time_saved >= threshold {
-                    pt1 += 1;
-                }
-            }
-        }
+    // For each position that has been visited, check which of the other visited positions can be
+    // reached within the cheat time (by moving freely anywhere on the grid, not just along points
+    // part of the path). For each of these reachable positions, check the time saving (if any).
+    let mut res = 0;
+    for (pos, cost) in visited.iter() {
+        for other in points_within_distance(*pos, cheat_time) {
+            // Check if the other point is reachable from course
+            if let Some(other_cost) = visited.get(&other) {
+                let d = distance(&pos, &other);
 
-        if wall_tile.1 > 0 {
-            if let Some(time_saved) = time_saved_by_shortcut(
-                &(wall_tile.0, wall_tile.1 - 1),
-                &(wall_tile.0, wall_tile.1 + 1),
-                &visited,
-            ) {
-                if time_saved >= threshold {
-                    pt1 += 1;
+                // We can only save time if the other point has a lower cost than this point AND
+                // this difference in cost is greater than the distance travelled via the shortcut
+                let new_other_cost = other_cost + d as u32;
+                if new_other_cost < *cost {
+                    let time_saved = cost - new_other_cost;
+                    if time_saved >= threshold {
+                        res += 1;
+                    }
                 }
             }
         }
     }
 
-    (pt1, 0)
+    res
 }
 
-fn time_saved_by_shortcut(
-    a: &(usize, usize),
-    b: &(usize, usize),
-    visited: &HashMap<(usize, usize), u32>,
-) -> Option<u32> {
-    if let Some(ta) = visited.get(a) {
-        if let Some(tb) = visited.get(b) {
-            let diff = ta.abs_diff(*tb);
-            let time_saved = diff.checked_sub(2).unwrap(); // diff should be > 2
-            return Some(time_saved);
-        }
-    }
+fn distance(a: &(usize, usize), b: &(usize, usize)) -> usize {
+    a.0.abs_diff(b.0) + a.1.abs_diff(b.1)
+}
 
-    None
+fn points_within_distance(
+    (x, y): (usize, usize),
+    distance: usize,
+) -> impl Iterator<Item = (usize, usize)> {
+    let miny = y.saturating_sub(distance);
+    let maxy = y.saturating_add(distance);
+
+    (miny..=maxy).flat_map(move |ny| {
+        let rem = distance - y.abs_diff(ny);
+        let minx = x.saturating_sub(rem);
+        let maxx = x.saturating_add(rem);
+        (minx..=maxx).map(move |nx| (nx, ny))
+    })
 }
 
 #[cfg(test)]
@@ -135,8 +133,46 @@ mod test {
     #[test]
     fn test_example() {
         let input = include_str!("../../inputs/20.ex");
-        let (pt1, pt2) = run_inner(&input, 10);
+        let pt1 = run_inner(&input, 2, 10);
         assert_eq!(pt1, 10);
-        assert_eq!(pt2, 0);
+
+        let pt2 = run_inner(&input, 20, 50);
+        assert_eq!(pt2, 285);
+    }
+
+    #[test]
+    fn test_points_within_distance() {
+        let mut actual: Vec<(usize, usize)> = points_within_distance((2, 2), 1).collect();
+        actual.sort();
+        let mut expected = vec![(1, 2), (2, 2), (3, 2), (2, 1), (2, 3)];
+        expected.sort();
+        assert_eq!(actual, expected);
+
+        let mut actual: Vec<(usize, usize)> = points_within_distance((2, 2), 2).collect();
+        actual.sort();
+
+        // Starting point is '.', reachable is '#', not reachable is 'x'
+        //   x x # x x
+        //   x # # # x
+        //   # # . # #
+        //   x # # # x
+        //   x x # x x
+        let mut expected = vec![
+            (2, 0),
+            (1, 1),
+            (2, 1),
+            (3, 1),
+            (0, 2),
+            (1, 2),
+            (2, 2),
+            (3, 2),
+            (4, 2),
+            (1, 3),
+            (2, 3),
+            (3, 3),
+            (2, 4),
+        ];
+        expected.sort();
+        assert_eq!(actual, expected);
     }
 }
